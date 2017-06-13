fab4f1c7d2dd0ba3765d6c8df0a3e396935559e3
Parnian <parnian@192.168.2.38>
2017-06-13 19:04:22 +0200

    new task added to gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
+var gulpLoadPlugins = require('gulp-load-plugins');
+var plugins = gulpLoadPlugins();
 
-gulp.task('hello-world', function(){
-    console.log('hello world');
+gulp.task('hello', function(){
+    console.log('hello Parnian! You are learning now GULP!');
 });
